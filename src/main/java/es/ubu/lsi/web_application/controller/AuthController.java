81e1c3dd167e277dedf59a28917b9605ab5bfb29
AitorBlanco03 <150005787+AitorBlanco03@users.noreply.github.com>
2025-04-17 18:39:36 +0200

    [FEAT] Navegación de la página principal al inicio de sesión.
    Implementación de la navegación desde la página principal hasta la página de inicio de sesión usando controladores y Thymeleaf.

---
+++ src/main/java/es/ubu/lsi/web_application/controller/AuthController.java
@@ -0,0 +1,31 @@
+package es.ubu.lsi.web_application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controlador web encargado de gestionar el flujo de autentificación y registro de usuarios
+ * dentro de nuestra aplicación.
+ *
+ * Se encarga de mostrar las páginas relacionadas con el inicio de sesión y el registro de
+ * usuarios. Además, gestiona la lógica necesaria para autentificar a los usuarios y procesar
+ * el registro de nuevos usuarios dentro del sistema.
+ *
+ * @author Aitor Blanco Fernández, abf1005@alu.ubu.es
+ * @version 1.0.0, 17 de Abril de 2026.
+ */
+
+@Controller
+public class AuthController {
+
+    /**
+     * Muestra por pantalla la página y el formulario de inicio de sesión
+     * de nuestra aplicación.
+     *
+     * @return el nombre de la página HTML que se debe de renderizar.
+     */
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+}
